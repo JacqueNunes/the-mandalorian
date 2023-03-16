6936a7d5eeb6706b83c4cc04e100faa2763667be
Jacqueline Nunes <115042952+JacqueNunes@users.noreply.github.com>
2023-03-15 22:17:43 -0300

    add new code js

--- src/js/index.js
+++ src/js/index.js
@@ -1,15 +1,40 @@
-const botoesCarrossel = document.querySelectorAll('.botao');
-const imagens = document.querySelectorAll('.imagem');
+const imagens = document.querySelectorAll('.carrossel img');
+const botoes = document.querySelectorAll('.botoes-carrossel button');
+let indice = 0;
 
-botoesCarrossel.forEach((botao, indice) => {
-	botao.addEventListener('click', () => {
-		const imagemAtual = document.querySelector('.ativa');
-		const botaoAtual = document.querySelector('.selecionado');
 
-		botaoAtual.classList.remove('selecionado');
-		imagemAtual.classList.remove('ativa');
+setInterval(() => {
+
+    imagens[indice].classList.remove('ativa');
+    indice = (indice + 1) % imagens.length;
+    imagens[indice].classList.add('ativa');
+    
+    botoes[indice].classList.remove('selecionado');
+	botoes[indice].classList.add('selecionado');
+   
+	
+}, 5000);
+
+
+
+
+
+
+
+
+
+// const botoesCarrossel = document.querySelectorAll('.botao');
+// const imagens = document.querySelectorAll('.imagem');
+
+// botoesCarrossel.forEach((botao, indice) => {
+// 	botao.addEventListener('click', () => {
+// 		const imagemAtual = document.querySelector('.ativa');
+// 		const botaoAtual = document.querySelector('.selecionado');
+
+// 		botaoAtual.classList.remove('selecionado');
+// 		imagemAtual.classList.remove('ativa');
         
-		imagens[indice].classList.add('ativa');
-		botoesCarrossel[indice].classList.add('selecionado');
-	});
-});
+// 		imagens[indice].classList.add('ativa');
+// 		botoesCarrossel[indice].classList.add('selecionado');
+// 	});
+// });
